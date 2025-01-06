450935361616335136bbe76b8a64ba6942602fd8
Ali <ali8551sbt@gmail.com>
2025-01-06 22:51:46 +0330

    modify home&dasbboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,10 +12,23 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <Button onClick={onClick} disabled={isPending}>
-        LogOut
-      </Button>
+    <div className="h-full bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* Radial gradient for the container to give a spotlight effect */}
+      <div className="relative h-full w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <div className="relative flex flex-col items-center justify-center">
+          <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            Dashboard
+          </h1>
+          <Button
+            onClick={onClick}
+            disabled={isPending}
+            className="relative border border-slate-800 bg-black text-white px-4 py-2 rounded-lg"
+          >
+            LogOut
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-full w-full flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white    p-8">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Our Application</h1>
+      <p className="text-lg mb-8 text-center max-w-md">
+        Experience the best features and seamless integration with our
+        cutting-edge application. Join us today and explore the possibilities.
+      </p>
       <Button asChild>
-        <Link href={"/sign-in"}>Sign in</Link>
+        <Link href={"/sign-in"} className="text-lg font-semibold">
+          Sign in
+        </Link>
       </Button>
     </div>
   );
